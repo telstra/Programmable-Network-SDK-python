c3e4a64099463b52970f48acf3fc98f20a64e6c7
developersteve <steven@developersteve.com>
2018-01-18 09:22:30 +1100

    SDK update

---
+++ docs/InventoryLinkRequest.md
@@ -0,0 +1,19 @@
+# InventoryLinkRequest
+
+## Properties
+Name | Type | Description | Notes
+------------ | ------------- | ------------- | -------------
+**billing_id** | **str** |  | [optional] 
+**description** | **str** |  | [optional] 
+**connections** | **list[str]** |  | [optional] 
+**tag** | **str** |  | [optional] 
+**latency** | **int** | Latency: 0&#x3D;Low, 1&#x3D;Standard, 2&#x3D;Best Effort | [optional] 
+**duration** | **int** | Duration of contract in minutes | [optional] 
+**bandwidth** | **int** | Bandwidth in kB/s | [optional] 
+**renewal_option** | **int** | Renewal Option: 0&#x3D;Auto Disconnect, 1&#x3D;Auto Renew, 2&#x3D;Pay per hour | [optional] 
+**link_type** | **int** | 1&#x3D;VLAN, 2&#x3D;VNF | [optional] 
+**topology_tag_uuid** | **str** |  | [optional] 
+
+[[Back to Model list]](../README.md#documentation-for-models) [[Back to API list]](../README.md#documentation-for-api-endpoints) [[Back to README]](../README.md)
+
+
--- test/test_eis100_endpoints_assign_topology_tag_request.py
+++ test/test_eis100_endpoints_assign_topology_tag_request.py
@@ -3,7 +3,7 @@
 """
     Telstra Programmable Network API
 
-    Telstra Programmable Network is a self-provisioning platform that allows its users to create on-demand connectivity services between multiple end-points and add various network functions to those services. Programmable Network enables to connectivity to a global ecosystem of networking services as well as public and private cloud services. Once you are connected to the platform on one or more POPs (points of presence), you can start creating those services based on the use case that you want to accomplish. The Programmable Network API is available to all customers who have registered to use the Programmable Network. To register, please contact your account representative.
+    Telstra Programmable Network is a self-provisioning platform that allows its users to create on-demand connectivity services between multiple end-points and add various network functions to those services. Programmable Network enables to connectivity to a global ecosystem of networking services as well as public and private cloud services. Once you are connected to the platform on one or more POPs (points of presence), you can start creating those services based on the use case that you want to accomplish. The Programmable Network API is available to all customers who have registered to use the Programmable Network. To register, please contact your account representative.  # noqa: E501
 
     OpenAPI spec version: 2.1.3
     
@@ -13,17 +13,15 @@
 
 from __future__ import absolute_import
 
-import os
-import sys
 import unittest
 
 import TelstraTPN
+from TelstraTPN.models.eis100_endpoints_assign_topology_tag_request import Eis100EndpointsAssignTopologyTagRequest  # noqa: E501
 from TelstraTPN.rest import ApiException
-from TelstraTPN.models.eis100_endpoints_assign_topology_tag_request import Eis100EndpointsAssignTopologyTagRequest
 
 
 class TestEis100EndpointsAssignTopologyTagRequest(unittest.TestCase):
-    """ Eis100EndpointsAssignTopologyTagRequest unit test stubs """
+    """Eis100EndpointsAssignTopologyTagRequest unit test stubs"""
 
     def setUp(self):
         pass
@@ -32,11 +30,9 @@ class TestEis100EndpointsAssignTopologyTagRequest(unittest.TestCase):
         pass
 
     def testEis100EndpointsAssignTopologyTagRequest(self):
-        """
-        Test Eis100EndpointsAssignTopologyTagRequest
-        """
+        """Test Eis100EndpointsAssignTopologyTagRequest"""
         # FIXME: construct object with mandatory attributes with example values
-        #model = TelstraTPN.models.eis100_endpoints_assign_topology_tag_request.Eis100EndpointsAssignTopologyTagRequest()
+        # model = TelstraTPN.models.eis100_endpoints_assign_topology_tag_request.Eis100EndpointsAssignTopologyTagRequest()  # noqa: E501
         pass
 
 
